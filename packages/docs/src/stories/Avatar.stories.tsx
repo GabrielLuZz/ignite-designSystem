36b2a231444d1ec1884c58d038d48b5a9218aee5
Gabriel Luz <gabrielluzsantosl@gmail.com>
2023-09-07 17:45:07 -0300

    Design System Components - Component: TextInput

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/GabrielLuZz.png',
+    alt: 'Gabriel Luz',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const withFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
