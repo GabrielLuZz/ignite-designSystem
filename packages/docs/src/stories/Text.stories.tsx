f83902076bc839a88889b92cd77d77b0e06445fe
Gabriel Luz <gabrielluzsantosl@gmail.com>
2023-09-07 10:04:21 -0300

    Design System Components - Component: Heading

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, quam consectetur accusantium cupiditate explicabo dignissimos nostrum dolorem commodi, corrupti sint est, nihil sed laborum esse veritatis adipisci officiis exercitationem qui?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
