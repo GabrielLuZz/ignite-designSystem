29f8b9d4643cb56d428a7f4b8b82955802fcbd3f
Gabriel Luz <gabrielluzsantosl@gmail.com>
2023-09-09 02:19:36 -0300

    Storybook Improvements - Component Controls

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/GabrielLuZz.png',
     alt: 'Gabriel Luz',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -5,8 +5,31 @@ export default {
   title: 'Typography/Text',
   component: Text,
   args: {
+    size: 'md',
     children:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, quam consectetur accusantium cupiditate explicabo dignissimos nostrum dolorem commodi, corrupti sint est, nihil sed laborum esse veritatis adipisci officiis exercitationem qui?',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
